3c5d0d2332694bc9ed0e7dceccaae6c6fa6f1582
sahbaz234 <110531834+mdsahbazkhan@users.noreply.github.com>
2024-07-09 22:21:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,21 @@ const express = require('express')
 var cors = require('cors')
 connectToMongo(); 
 const app = express()
-const port =  5006;
-const corsOptions = {
-  origin: 'https://inotebook49.netlify.app', // Replace with your frontend URL for local development
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
-};
+
+// const corsOptions = {
+//   origin: 'https://inotebook49.netlify.app', // Replace with your frontend URL for local development
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type'],
+// };
 
 // Apply CORS middleware
-app.use(cors(corsOptions);
+app.use(cors();
 
 app.use(express.json())
 //Available Routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
+const port =  process.env.PORT || 5006;
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening http://localhost:${port}`)
