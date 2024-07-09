0d18085164b2f46938987592cba70a2e5b605f3f
sahbaz234 <110531834+mdsahbazkhan@users.noreply.github.com>
2024-07-09 21:19:21 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToMongo = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/inotebook?directConnection=true")
+    .connect("mongodb+srv://sahbazkhan:23042003$ah@cluster0.p1amxlo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => {
       console.log("Connected to MongoDB");
     })
